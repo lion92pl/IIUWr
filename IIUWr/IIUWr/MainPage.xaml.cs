79f8505c7af1eb4dec6202945cb13170e345da5d
Tomasz Sołtysik <tomasz@soltysik.net.pl>
2016-04-10 17:49:37 +0200

    Added progress for loading data

--- IIUWr/IIUWr/MainPage.xaml.cs
+++ IIUWr/IIUWr/MainPage.xaml.cs
@@ -31,15 +31,19 @@ namespace IIUWr
 
             public async void RefreshSemesters()
             {
+                IsRefreshingSemesters = true;
                 await CoursesService.RefreshSemesters();
+                IsRefreshingSemesters = false;
             }
 
             public async void RefreshSelectedCourse()
             {
                 if (SelectedCourse != null)
                 {
+                    IsRefreshingCourse = true;
                     await CoursesService.RefreshCourse(SelectedCourse);
                     PropertyChanged.Notify(this, nameof(SelectedCourse));
+                    IsRefreshingCourse = false;
                 }
             }
 
@@ -54,6 +58,35 @@ namespace IIUWr
                     if (_selectedCourse != value)
                     {
                         _selectedCourse = value;
+                        PropertyChanged.Notify(this);
+                        RefreshSelectedCourse();
+                    }
+                }
+            }
+
+            private bool _isRefreshingCourse;
+            public bool IsRefreshingCourse
+            {
+                get { return _isRefreshingCourse; }
+                set
+                {
+                    if (_isRefreshingCourse != value)
+                    {
+                        _isRefreshingCourse = value;
+                        PropertyChanged.Notify(this);
+                    }
+                }
+            }
+
+            private bool _isRefreshingSemesters;
+            public bool IsRefreshingSemesters
+            {
+                get { return _isRefreshingSemesters; }
+                set
+                {
+                    if (_isRefreshingSemesters != value)
+                    {
+                        _isRefreshingSemesters = value;
                         PropertyChanged.Notify(this);
                     }
                 }
