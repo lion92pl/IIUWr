6a43bf9d43d0addbd340b06ad74e29c6fedc4272
Tomasz Sołtysik <tomasz@soltysik.net.pl>
2016-05-07 20:18:45 +0200

    WebView color invert test

--- IIUWr/IIUWr/MainPage.xaml.cs
+++ IIUWr/IIUWr/MainPage.xaml.cs
@@ -4,16 +4,19 @@ using IIUWr.ViewModelInterfaces.Fereol;
 using LionCub.Patterns.DependencyInjection;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
+using System;
 using System.Threading.Tasks;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI;
 
 namespace IIUWr
 {
     /// <summary>Main page</summary>
     public sealed partial class MainPage : Page
     {
+        private bool inverted = false;
+
         public MainPage()
         {
             InitializeComponent();
@@ -24,6 +27,16 @@ namespace IIUWr
             base.OnNavigatedTo(e);
 
             DataContext = e.Parameter;
+        }
+
+        private async void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            //TODO create AttachedProperty for invert
+            inverted = !inverted;
+            webView.DefaultBackgroundColor = inverted ? Colors.Black : Colors.White;
+            int percent = inverted ? 100 : 0;
+            string func = $"document.firstChild.style['filter'] = 'invert({percent}%)';";
+            await webView.InvokeScriptAsync("eval", new string[] { func });
         }
     }
 }
