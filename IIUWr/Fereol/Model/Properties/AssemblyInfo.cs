a417ecd4a2d00c920ccf77e9f9e4721aae2eb377
Tomasz Sołtysik <tomasz@soltysik.net.pl>
2017-08-27 00:52:48 +0200

    AssemblyInfo

--- IIUWr/Fereol/Model/Properties/AssemblyInfo.cs
+++ IIUWr/Fereol/Model/Properties/AssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("IIUWr.Fereol.Model")]
 [assembly: AssemblyProduct("Fereol Model")]
-[assembly: AssemblyCopyright("Copyright © Tomasz Sołtysik 2016")]
+[assembly: AssemblyCopyright("Copyright © Tomasz Sołtysik 2017")]
 
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.3.*")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
 [assembly: ComVisible(false)]
